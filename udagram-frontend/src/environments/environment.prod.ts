02bd82d000d3749e54fb3a195cdf82e98f2de3c2
LongCa <thanhlong1543@gmail.com>
2022-09-10 20:20:49 +0700

    update elb host name

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1002bf7dd9d2422a9bde34013fca772-75263288.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
