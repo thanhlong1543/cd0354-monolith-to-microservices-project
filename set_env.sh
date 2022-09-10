6f2796ade153be979a1f0c6b940dd92986955a2a
LongCa <thanhlong1543@gmail.com>
2022-09-10 16:45:25 +0700

    refactor

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+set POSTGRES_USERNAME=longca
+set POSTGRES_PASSWORD=823082aa
+set POSTGRES_HOST=cdr.c9y5xtq2rbus.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET=arn:aws:s3:::my-longca
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'longca',
+  'password': '823082aa',
+  'database': 'postgres',
+  'host': 'cdr.c9y5xtq2rbus.us-east-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'my-longca',
+  'url': 'http://localhost:8100',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': 'testing',
   },
 };
